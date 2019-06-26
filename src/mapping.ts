8c69c00fc30c0a8c4f4257a1580b34dee1f57939
Zefram Lou <zeframlou@gmail.com>
2019-06-26 17:23:10 +0800

    initial commit

--- src/mapping.ts
+++ src/mapping.ts
@@ -16,204 +16,218 @@ import {
   ProposedCandidate as ProposedCandidateEvent,
   Voted as VotedEvent,
   FinalizedNextVersion as FinalizedNextVersionEvent,
-  OwnershipTransferred as OwnershipTransferredEvent
-} from "../generated/Contract/Contract"
+  OwnershipTransferred as OwnershipTransferredEvent,
+  BetokenFund
+} from "../generated/BetokenFund/BetokenFund"
+
 import {
-  ChangedPhase,
-  Deposit,
-  Withdraw,
-  CreatedInvestment,
-  SoldInvestment,
-  CreatedCompoundOrder,
-  SoldCompoundOrder,
-  RepaidCompoundOrder,
-  CommissionPaid,
-  TotalCommissionPaid,
-  Register,
-  SignaledUpgrade,
-  DeveloperInitiatedUpgrade,
-  InitiatedUpgrade,
-  ProposedCandidate,
-  Voted,
-  FinalizedNextVersion,
-  OwnershipTransferred
+  Manager,
+  BasicOrder,
+  CompoundOrder,
+  FulcrumOrder,
+  CommissionRedemption,
+  Investor,
+  DepositWithdraw,
+  Fund,
+  DataPoint
 } from "../generated/schema"
 
+import { BigInt } from '@graphprotocol/graph-ts'
+
+enum CyclePhase {
+  INTERMISSION,
+  MANAGE
+}
+
+enum VoteDirection {
+  EMPTY,
+  FOR,
+  AGAINST
+}
+
+// Constants
+
+const PTOKENS = require('./fulcrum_tokens.json');
+const RISK_THRESHOLD_TIME = BigInt.fromI32(3 * 24 * 60 * 60) // 3 days, in seconds
+const ZERO = BigInt.fromI32(0)
+
+// Helpers
+
+export const isUndefined = (x) => x == null;
+
+export const isFulcrumTokenAddress = (_tokenAddress) => {
+  const result = PTOKENS.find((x) => !isUndefined(x.pTokens.find((y) => y.address === _tokenAddress)));
+  return !isUndefined(result);
+}
+
+export const assetPTokenAddressToInfo = (_addr) => {
+  return PTOKENS.find((x) => !isUndefined(x.pTokens.find((y) => y.address === _addr))).pTokens.find((y) => y.address === _addr);
+}
+
+// Handlers
+
 export function handleChangedPhase(event: ChangedPhaseEvent): void {
-  let entity = new ChangedPhase(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._cycleNumber = event.params._cycleNumber
-  entity._newPhase = event.params._newPhase
-  entity._timestamp = event.params._timestamp
-  entity._totalFundsInDAI = event.params._totalFundsInDAI
+  let entity = Fund.load("0");
+  entity.cycleNumber = event.params._cycleNumber
+  entity.cyclePhase = CyclePhase[event.params._newPhase.toI32()]
+  if (!entity.hasFinalizedNextVersion) {
+    entity.candidates.length = 0
+    entity.proposers.length = 0
+    entity.forVotes.length = 0
+    entity.againstVotes.length = 0
+    entity.upgradeVotingActive = false
+    entity.upgradeSignalStrength = ZERO
+    entity.nextVersion = ""
+  }
   entity.save()
 }
 
 export function handleDeposit(event: DepositEvent): void {
-  let entity = new Deposit(
+  let entity = new DepositWithdraw(
     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
   )
-  entity._cycleNumber = event.params._cycleNumber
-  entity._sender = event.params._sender
-  entity._tokenAddress = event.params._tokenAddress
-  entity._tokenAmount = event.params._tokenAmount
-  entity._daiAmount = event.params._daiAmount
-  entity._timestamp = event.params._timestamp
+  entity.amountInDAI = event.params._daiAmount
+  entity.timestamp = event.params._timestamp
+  entity.isDeposit = true
+  entity.txHash = event.transaction.hash.toHex()
   entity.save()
 }
 
 export function handleWithdraw(event: WithdrawEvent): void {
-  let entity = new Withdraw(
+  let entity = new DepositWithdraw(
     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
   )
-  entity._cycleNumber = event.params._cycleNumber
-  entity._sender = event.params._sender
-  entity._tokenAddress = event.params._tokenAddress
-  entity._tokenAmount = event.params._tokenAmount
-  entity._daiAmount = event.params._daiAmount
-  entity._timestamp = event.params._timestamp
+  entity.amountInDAI = event.params._daiAmount
+  entity.timestamp = event.params._timestamp
+  entity.isDeposit = false
+  entity.txHash = event.transaction.hash.toHex()
   entity.save()
 }
 
 export function handleCreatedInvestment(event: CreatedInvestmentEvent): void {
-  let entity = new CreatedInvestment(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._cycleNumber = event.params._cycleNumber
-  entity._sender = event.params._sender
-  entity._id = event.params._id
-  entity._tokenAddress = event.params._tokenAddress
-  entity._stakeInWeis = event.params._stakeInWeis
-  entity._buyPrice = event.params._buyPrice
-  entity._costDAIAmount = event.params._costDAIAmount
+  let id = event.params._id.toString() + '-' + event.params._cycleNumber.toString()
+  let entity;
+  if (isFulcrumTokenAddress(event.params._tokenAddress)) {
+    entity = new FulcrumOrder(id);
+    entity.isShort = assetPTokenAddressToInfo(event.params._tokenAddress).type;
+  } else {
+    entity = new BasicOrder(id);
+  }
+  entity.idx = event.params._id
+  entity.cycleNumber = event.params._cycleNumber
+  entity.tokenAddress = event.params._tokenAddress
+  entity.stake = event.params._stakeInWeis
+  entity.buyPrice = event.params._buyPrice
+  entity.buyTime = event.block.timestamp
   entity.save()
+
+  let manager = Manager.load(event.params._sender.toHex())
+  if (isFulcrumTokenAddress(event.params._tokenAddress)) {
+    manager.fulcrumOrders.push(entity.id)
+  } else {
+    manager.basicOrders.push(entity.id)
+  }
+  manager.save()
 }
 
 export function handleSoldInvestment(event: SoldInvestmentEvent): void {
-  let entity = new SoldInvestment(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._cycleNumber = event.params._cycleNumber
-  entity._sender = event.params._sender
-  entity._investmentId = event.params._investmentId
-  entity._receivedKairo = event.params._receivedKairo
-  entity._sellPrice = event.params._sellPrice
-  entity._earnedDAIAmount = event.params._earnedDAIAmount
+  let id = event.params._id.toString() + '-' + event.params._cycleNumber.toString()
+  let entity;
+  if (isFulcrumTokenAddress(event.params._tokenAddress)) {
+    entity = FulcrumOrder.load(id);
+  } else {
+    entity = BasicOrder.load(id);
+  }
+  entity.isSold = true
   entity.save()
 }
 
 export function handleCreatedCompoundOrder(
   event: CreatedCompoundOrderEvent
 ): void {
-  let entity = new CreatedCompoundOrder(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._cycleNumber = event.params._cycleNumber
-  entity._sender = event.params._sender
-  entity._order = event.params._order
-  entity._orderType = event.params._orderType
-  entity._tokenAddress = event.params._tokenAddress
-  entity._stakeInWeis = event.params._stakeInWeis
-  entity._costDAIAmount = event.params._costDAIAmount
+  let id = event.params._id.toString() + '-' + event.params._cycleNumber.toString()
+  let entity = new CompoundOrder(id)
+  entity.idx = event.params._id
+  entity.cycleNumber = event.params._cycleNumber
+  entity.tokenAddress = event.params._tokenAddress.toHex()
+  entity.stake = event.params._stakeInWeis
+  entity.collateralAmountInDAI = event.params._costDAIAmount
+  entity.buyTime = event.block.timestamp
+  entity.isShort = event.params._orderType
   entity.save()
+
+  let manager = Manager.load(event.params._sender.toHex())
+  manager.compoundOrders.push(entity.id)
+  manager.save()
 }
 
 export function handleSoldCompoundOrder(event: SoldCompoundOrderEvent): void {
-  let entity = new SoldCompoundOrder(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._cycleNumber = event.params._cycleNumber
-  entity._sender = event.params._sender
-  entity._order = event.params._order
-  entity._orderType = event.params._orderType
-  entity._tokenAddress = event.params._tokenAddress
-  entity._receivedKairo = event.params._receivedKairo
-  entity._earnedDAIAmount = event.params._earnedDAIAmount
-  entity.save()
-}
-
-export function handleRepaidCompoundOrder(
-  event: RepaidCompoundOrderEvent
-): void {
-  let entity = new RepaidCompoundOrder(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._cycleNumber = event.params._cycleNumber
-  entity._sender = event.params._sender
-  entity._order = event.params._order
-  entity._repaidDAIAmount = event.params._repaidDAIAmount
+  let id = event.params._id.toString() + '-' + event.params._cycleNumber.toString()
+  let entity = CompoundOrder.load(id)
+  entity.isSold = true
   entity.save()
 }
 
 export function handleCommissionPaid(event: CommissionPaidEvent): void {
-  let entity = new CommissionPaid(
+  let entity = new CommissionRedemption(
     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
   )
-  entity._cycleNumber = event.params._cycleNumber
-  entity._sender = event.params._sender
-  entity._commission = event.params._commission
+  entity.timestamp = event.block.timestamp
+  entity.cycleNumber = event.params._cycleNumber
+  entity.amountInDAI = event.params._commission
+  entity.txHash = event.transaction.hash.toHex()
   entity.save()
-}
 
-export function handleTotalCommissionPaid(
-  event: TotalCommissionPaidEvent
-): void {
-  let entity = new TotalCommissionPaid(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._cycleNumber = event.params._cycleNumber
-  entity._totalCommissionInDAI = event.params._totalCommissionInDAI
-  entity.save()
+  let manager = Manager.load(event.params._sender.toHex())
+  manager.commissionHistory.push(entity.id)
+  manager.save()
 }
 
 export function handleRegister(event: RegisterEvent): void {
-  let entity = new Register(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._manager = event.params._manager
-  entity._block = event.params._block
-  entity._donationInDAI = event.params._donationInDAI
+  let entity = new Manager(event.params._manager.toHex())
+  entity.kairoBalance = event.params._kairoReceived
+  entity.kairoBalanceWithStake = event.params._kairoReceived
+  entity.baseStake = event.params._kairoReceived
+  entity.riskTaken = ZERO
+  entity.riskThreshold = entity.baseStake.times(RISK_THRESHOLD_TIME)
+  entity.lastCommissionRedemption = ZERO
+  entity.upgradeSignal = false
   entity.save()
 }
 
 export function handleSignaledUpgrade(event: SignaledUpgradeEvent): void {
-  let entity = new SignaledUpgrade(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._cycleNumber = event.params._cycleNumber
-  entity._sender = event.params._sender
-  entity._inSupport = event.params._inSupport
-  entity.save()
+  let manager = Manager.load(event.params._sender.toHex())
+  manager.upgradeSignal = event.params._inSupport
+  manager.save()
 }
 
 export function handleDeveloperInitiatedUpgrade(
   event: DeveloperInitiatedUpgradeEvent
 ): void {
-  let entity = new DeveloperInitiatedUpgrade(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._cycleNumber = event.params._cycleNumber
-  entity._candidate = event.params._candidate
+  let entity = Fund.load("0")
+  entity.upgradeVotingActive = true
+  entity.nextVersion = event.params._candidate.toHex()
   entity.save()
 }
 
 export function handleInitiatedUpgrade(event: InitiatedUpgradeEvent): void {
-  let entity = new InitiatedUpgrade(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._cycleNumber = event.params._cycleNumber
+  let entity = Fund.load("0")
+  entity.upgradeVotingActive = true
   entity.save()
 }
 
 export function handleProposedCandidate(event: ProposedCandidateEvent): void {
-  let entity = new ProposedCandidate(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._cycleNumber = event.params._cycleNumber
-  entity._voteID = event.params._voteID
-  entity._sender = event.params._sender
-  entity._candidate = event.params._candidate
+  let entity = Fund.load("0")
+  let voteID = event.params._voteID
+  if (voteID.gt(BigInt.fromI32(entity.candidates.length - 1))) {
+    // candidate for new chunk, push
+    entity.candidates.push(event.params._candidate.toHex())
+    entity.proposers.push(event.params._sender.toHex())
+  } else {
+    // update current chunk
+    entity.candidates[voteID.toI32()] = event.params._candidate.toHex()
+    entity.proposers[voteID.toI32()] = event.params._sender.toHex()
+  }
   entity.save()
 }
 
