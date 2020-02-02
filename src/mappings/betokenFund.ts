3162d90ad5434505acb944f1f9577a84eb87f13b
Zefram Lou <zeframlou@gmail.com>
2020-02-02 11:19:19 -0800

    added kairoBalance to ManagerROI

--- src/mappings/betokenFund.ts
+++ src/mappings/betokenFund.ts
@@ -90,8 +90,8 @@ export function handleChangedPhase(event: ChangedPhaseEvent): void {
       let roi = new ManagerROI(manager.id + '-' + entity.cycleNumber.toString())
       roi.manager = manager.id
       roi.cycle = entity.cycleNumber.minus(BigInt.fromI32(1))
-      log.info("ROI: manager: {}, kairoBalance: {}, baseStake: {}", [manager.id, manager.kairoBalance.toString(), manager.baseStake.toString()])
       roi.roi = manager.baseStake.equals(Utils.ZERO_DEC) ? Utils.ZERO_DEC : manager.kairoBalance.div(manager.baseStake).minus(BigDecimal.fromString('1')).times(BigDecimal.fromString('100'))
+      roi.kairoBalance = manager.kairoBalance
       roi.save()
       let roiHistory = manager.roiHistory
       roiHistory.push(roi.id)
